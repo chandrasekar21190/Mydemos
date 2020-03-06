b11f0346eb750357aabcb79bb2cea6ef2e645261
chandrasekar21190 <61863272+chandrasekar21190@users.noreply.github.com>
2020-03-06 15:32:14 +0530

    My project

---
+++ index.php
@@ -0,0 +1,79 @@
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  
+</head>
+<!-- Page Content -->
+<div class="container">
+
+
+  <hr class="mt-2 mb-5">
+
+  <div class="row text-center text-lg-left">
+
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+      </a><p class='text-center'>NGS</p>
+		  
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+      <!--   <iframe width="400" height="300" src="https://www.youtube.com/embed/L5sWnh3EkTI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="" width='400' height='300'>
+          </a><p class='text-center'>Microarray</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Capillary Sequencing</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Real Time PCR (RT-PCR)</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Gene Xpert</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Flow Cytometry</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="" width='400' height='300'>
+          </a><p class='text-center'>Immunofluorescence</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Circulating Tumor Cells</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>FISH</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Karyotyping</p>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6 ">
+      <a href="#" class="d-block mb-4 h-100">
+            <img class="img-fluid img-thumbnail" src="images/ngs.jpg" alt="">
+          </a><p class='text-center'>Portable Laser for Embryo Biopsy</p>
+    </div>
+  </div>
+
+</div>
+<!-- /.container -->
